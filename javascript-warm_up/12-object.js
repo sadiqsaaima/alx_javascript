a8865b5071f69135a113f5b77ecd4135bce24ba1
sadiqsaaima <saaimadole@gmail.com>
2023-09-02 02:45:13 -0700

    school

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,7 +1,8 @@
-#!/usr/bin/node
+const favlang = ["C is fun", "Python is cool", "JavaScript is amazing"];
 
-let name = 12;
-
-name = 89;
-
-console.log(name);
+for (let i = 0; i < favlang.length; i++) {
+  if (favlang[i].includes('12')) {
+    favlang[i] = favlang[i].replace('12', '89');
+  }
+  console.log(favlang[i]);
+}
